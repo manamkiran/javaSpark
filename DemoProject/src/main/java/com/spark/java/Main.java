8a7cb7045768f80cd8b15dfe83a60845c8ac9f1b
manamkiran <manamkiranyadav@gmail.com>
2021-04-13 04:50:53 +0530

    Reading from a file and getting top words

--- DemoProject/src/main/java/com/spark/java/Main.java
+++ DemoProject/src/main/java/com/spark/java/Main.java
@@ -1,34 +1,34 @@
 package com.spark.java;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import scala.Tuple2;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		List<String> inputData = new ArrayList<>();
-		inputData.add("WARN: Tuesday 4");
-		inputData.add("ERROR: Tuesday 4");
-		inputData.add("FATAL: Wednesday 5");
-		inputData.add("ERROR: Friday 7");
-		inputData.add("WARN: Saturday 8");
+		System.setProperty("hadoop.home.dir", "D:\\softwares\\hadoop");
 
 		Logger.getLogger("org.apache").setLevel(Level.WARN);
 
 		SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
 
 		try (JavaSparkContext sc = new JavaSparkContext(conf);) {
-
-			sc.parallelize(inputData).flatMap(value -> Arrays.asList(value.split(" ")).iterator())
-//					.foreach(System.out::println); //Not working
-					.foreach(word -> System.out.println(word));
+			sc.textFile("src/main/resources/subtitles/input.txt")
+					.map(sentence -> sentence.toLowerCase().replaceAll("[^a-zA-Z\\s]", ""))
+					.filter(sentence -> sentence.trim().length() > 1)
+					.flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator())
+					.filter(word -> word.trim().length() > 1).filter(Util::isNotBoring)
+					.mapToPair(word -> new Tuple2<>(word, 1))
+					.reduceByKey((value1, value2) -> value1 + value2)
+					.mapToPair(tuple -> new Tuple2<>(tuple._2, tuple._1)).sortByKey(false).take(10)
+					.forEach(countTuple -> System.out.println(countTuple._2 +" word has occured " + countTuple._1));
 			;
 		}
 	}
