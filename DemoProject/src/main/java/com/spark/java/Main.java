189fc4bb6ab7531ff4f5db138be659d1e23a9d0d
manamkiran <manamkiranyadav@gmail.com>
2021-04-20 14:51:24 +0530

    Added new log file for processing and grouping it level,month and using Java api to drop a column from view

--- DemoProject/src/main/java/com/spark/java/Main.java
+++ DemoProject/src/main/java/com/spark/java/Main.java
@@ -1,19 +1,10 @@
 package com.spark.java;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataType;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.Metadata;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
 
 public class Main {
 
@@ -26,28 +17,30 @@ public class Main {
 		try (SparkSession spark = SparkSession.builder().appName("testingSQL").master("local[*]")
 				.config("spark.sql.warehouse.dir", "file:///D:/softwares/hadoop").getOrCreate()) {
 
-			// Dataset<Row> dataset = spark.read().option("header",
-			// true).csv("src/main/resources/exams/students.csv");
-
-			List<Row> inMemory = new ArrayList<Row>();
-			inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
-			inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
-			inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
-			inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
-			inMemory.add(RowFactory.create("FATAL", "2015-4-21 19:23:20"));
-
-			StructField[] fields = new StructField[] {
-					new StructField("level", DataTypes.StringType, false, Metadata.empty()),
-					new StructField("datetime", DataTypes.StringType, false, Metadata.empty()) };
-
-			StructType schema = new StructType(fields);
-			Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
+			Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/extras/biglog.txt");
 
 			dataset.createOrReplaceTempView("loggingTable");
 
-			spark.sql("select level,date_format(datetime,'MMMM') as month,count(*) as total from loggingTable group by level,month").show();
+			Dataset<Row> results = spark.sql(
+					"select level,date_format(datetime,'MMMM') as month,first(date_format(datetime,'MM')) as monthNum,count(*) as total from loggingTable group by level,month order by monthNum");
 
+			results.drop("monthNum").show(100);
 //			dataset.show();
+			/*
+			 * List<Row> inMemory = new ArrayList<Row>();
+			 * inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+			 * inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+			 * inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+			 * inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
+			 * inMemory.add(RowFactory.create("FATAL", "2015-4-21 19:23:20"));
+			 * 
+			 * StructField[] fields = new StructField[] { new StructField("level",
+			 * DataTypes.StringType, false, Metadata.empty()), new StructField("datetime",
+			 * DataTypes.StringType, false, Metadata.empty()) };
+			 * 
+			 * StructType schema = new StructType(fields); Dataset<Row> dataset =
+			 * spark.createDataFrame(inMemory, schema);
+			 */
 		}
 
 		/*
